afa9db16647481afee0b30806d1c5f62c3eda456
Sergies Baptista de Oliveira <sergiesoliveira@gmail.com>
2026-02-26 11:06:58 -0300

    aula 7

--- civillaw_main/views.py
+++ civillaw_main/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
+from .models import Cliente
 
 # Create your views here.
 
 def index(request):
     """ pagina principal"""
+    return render(request, 'civillaw_main/index.html')
+
+def clientes(request):
+    """ lista dos os clientes cadastrados """
+    cliente = Cliente.objects.order_by('clie_nome')
+    context = {'clientes': clientes}
     return render(request, 'civillaw_main/index.html')
